d330807bfb047064040451b5cccf4a4aadd62ee9
Rezaofc4 <106759741+Rezaofc4@users.noreply.github.com>
2022-06-05 20:52:08 +0700

    Create main-welkom.js

---
+++ plugins/main-welkom.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let name = await conn.getName(m.sender)
+  let wm = global.wm
+hyzer = {
+    key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+    },
+  message: { 
+  "extendedTextMessage": {
+  "text": '*Halo dek. Ketik .menu untuk memulai bot*',
+  "title": wm,
+  'jpegThumbnail': 'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
+        }
+       } 
+     }
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/6f1c71d4f801482c930b8.png', hyzer, { packname: "sticker by", author: "hyzer" })
+}
+
+handler.customPrefix = /^(Hi Beban 👋)$/i
+handler.command = new RegExp
+
+module.exports = handler
